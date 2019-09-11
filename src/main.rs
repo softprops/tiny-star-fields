c244fcf2a42d10457790940202e0996955bd8630
softprops <d.tangren@gmail.com>
2019-09-12 00:55:37 +0900

    rustdoc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+//! A random start field generated inspired by artwork by [@tiny_star_fields](https://twitter.com/tiny_star_field) on Twitter
 use rand::{seq::SliceRandom, Rng};
 use std::iter::repeat_with;
 
 #[rustfmt::skip]
 const STARS: &[&str] = &["* ", " . ", " . ", ". ", " + ", " * ", "✵  ", "✷  ", "* ", " . ", "* ", ".", " . ", "    ", " ˚ ", " ·", " · ", "· ", " ˚ ", " · ", " ·", "·  ", ".", ". ", " ✧", " ✫ ", " ⊹ ", " *", " ⋆ ", " ✺ ", " ✹ ", " ✵ ", " ˚ ", " ✦ ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " 　", " ", "", " ", " ", " ", "　  　", " ", " ", " ", " ", " ", "　", "　", " 　", "　", " 　　", "　", "  　", "　", " 　", "　", "　　", "　", "　", "　　", "　", "　", "　 ", "　　", "　", "　　", "　　　", "　", "　", "　　", "　", "　", "　　　", "　", "　", "　　", "　", "　　　　", "　", "　　　　　", "　　 ", "　　 ", "　 ", " ", "　　　　", "  ", " ", "     ", "   ", "     ", "   ", " "];
 
+/// Return an Iterator over random ascii star field text
 fn stars() -> impl Iterator<Item = String> {
     let mut rng = rand::thread_rng();
     repeat_with(move || {
