062f4683274d233645ba0d2751fbffafb35da260
softprops <d.tangren@gmail.com>
2020-09-07 11:51:57 -0400

    parameterize

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 //! A random start field inspired by artwork by [@tiny_star_fields](https://twitter.com/tiny_star_field) on Twitter
 use rand::{seq::SliceRandom, Rng};
 use std::iter::repeat_with;
+use structopt::StructOpt;
 
 #[rustfmt::skip]
 const STARS: &[&str] = &["* ", " . ", " . ", ". ", " + ", " * ", "✵  ", "✷  ", "* ", " . ", "* ", ".", " . ", "    ", " ˚ ", " ·", " · ", "· ", " ˚ ", " · ", " ·", "·  ", ".", ". ", " ✧", " ✫ ", " ⊹ ", " *", " ⋆ ", " ✺ ", " ✹ ", " ✵ ", " ˚ ", " ✦ ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " 　", " ", "", " ", " ", " ", "　  　", " ", " ", " ", " ", " ", "　", "　", " 　", "　", " 　　", "　", "  　", "　", " 　", "　", "　　", "　", "　", "　　", "　", "　", "　 ", "　　", "　", "　　", "　　　", "　", "　", "　　", "　", "　", "　　　", "　", "　", "　　", "　", "　　　　", "　", "　　　　　", "　　 ", "　　 ", "　 ", " ", "　　　　", "  ", " ", "     ", "   ", "     ", "   ", " "];
 
+#[derive(Debug, StructOpt)]
+struct Options {
+    #[structopt(default_value="5", long, short)]
+    columns: usize,
+}
+
+
 /// Return an Iterator over random ascii star field text
-fn stars() -> impl Iterator<Item = String> {
+fn stars(options: Options) -> impl Iterator<Item = String> {
+    let Options { columns } = options;
     let mut rng = rand::thread_rng();
     repeat_with(move || {
-        let take = (rng.gen::<f32>() * 5.0).floor() as usize + 5;
+        let take = (rng.gen::<f32>() * columns as f32).floor() as usize + columns;
         STARS
             .choose_multiple(&mut rng, take)
             .fold(String::new(), |res, line| res + line)
@@ -17,7 +26,8 @@ fn stars() -> impl Iterator<Item = String> {
 }
 
 fn main() {
-    for line in stars().take(100) {
+    let options = Options::from_args();
+    for line in stars(options).take(100) {
         println!("{}", line);
     }
 }
